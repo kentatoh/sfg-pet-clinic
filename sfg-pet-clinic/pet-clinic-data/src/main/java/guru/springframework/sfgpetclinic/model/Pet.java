33652e0a8ec982644ddf41a06d67211097ace579
Kenta <58523090+kentatoh@users.noreply.github.com>
2022-07-08 11:40:54 +0800

    Added PetType SD JPA Service. Closes #42

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -14,7 +14,7 @@ public class Pet extends BaseEntity {
     private PetType petType;
 
     @ManyToOne
-    @Column(name = "owner_id")
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     @Column(name = "birth_date")
